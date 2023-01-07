c8060c04bb4a6fa8136c456481dd60e1196b5018
vanemvilte90 <vanemvilte90@gmail.com>
2023-01-07 00:06:27 -0300

    Agrego base del proyecto: assets y components

---
+++ react-portfolio/src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <h5>Hola, soy</h5> 
+        <h1>Vane Vilte</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+      </div>
+
+  
+
+    </header>
+  )
+}
+
+export default Header
