9f3df9fc06573970222896e6f2c1a0e1a2c85e13
vanemvilte90 <vanemvilte90@gmail.com>
2023-01-13 22:56:12 -0300

    Se agrega sección portfolio y testimonios

--- react-portfolio/src/components/portfolio/Portfolio.jsx
+++ react-portfolio/src/components/portfolio/Portfolio.jsx
@@ -56,7 +56,7 @@ const data = [
 const Portfolio = () => {
   return (
     <section id='portfolio'>
-      <h5>My recent Work</h5>
+      <h5>Aquí puedes ver mi trabajo</h5>
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
--- react-portfolio/src/components/testimonials/Testimonials.jsx
+++ react-portfolio/src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,74 @@
-import React from 'react'
-import './testimonials.css'
+import React from 'react';
+import './testimonials.css';
+import AVTR1 from '../../assets/avatar1.jpg';
+import AVTR2 from '../../assets/avatar2.jpg';
+import AVTR3 from '../../assets/avatar3.jpg';
+import AVTR4 from '../../assets/avatar4.jpg';
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum suscipit at doloribus nulla id rerum? Aspernatur perferendis perspiciatis quidem esse quaerat molestias nobis dicta nam illo, excepturi dolore illum?'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Shata Wale',
+    review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum suscipit at doloribus nulla id rerum? Aspernatur perferendis perspiciatis quidem esse quaerat molestias nobis dicta nam illo, excepturi dolore illum?'
+  },
+  {
+    avatar: AVTR3,
+    name: 'Kwame Despite',
+    review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum suscipit at doloribus nulla id rerum? Aspernatur perferendis perspiciatis quidem esse quaerat molestias nobis dicta nam illo, excepturi dolore illum?'
+  },
+  {
+    avatar: AVTR4,
+    name: 'Nana Ama McBrown',
+    review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum suscipit at doloribus nulla id rerum? Aspernatur perferendis perspiciatis quidem esse quaerat molestias nobis dicta nam illo, excepturi dolore illum?'
+  },
+
+]
+
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Lo que mis clienten dicen...</h5>
+      <h2>Testimonios</h2>
+      <Swiper className="container testimonials__container"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {
+          data.map(({avatar, name, review}, index) => {
+            return(
+              <SwiperSlide key={index} className='testimonial'>
+                <div className="client__avatar">
+                  <img src={avatar} />
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>{review}</small>
+              </SwiperSlide>
+            )
+
+          })
+        }
+      </Swiper>
+    </section>
   )
 }
 
-export default Testimonials
+export default Testimonials;
